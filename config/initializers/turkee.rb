fe1d59ead52796f12d113181313db1882c0b8295
Jim Jones <jjones@aantix.com>
2012-12-03 22:01:15 -0800

    Updated url to reflect Heroku app.

--- config/initializers/turkee.rb
+++ config/initializers/turkee.rb
@@ -3,7 +3,7 @@
 AWS_ID  = ENV['AWS_ID'] || '1Q618FTH0XDX7RR6RW82'
 AWS_KEY = ENV['AWS_KEY'] || 'UBkMRPv+kgNwATyVnM8613Z9mOZ8pU2R/FbkXi23'
 
-TURKEE_URL = Rails.env == 'production' ? "https://www.thinktraction.com" : "http://localhost:3000"
+TURKEE_URL = Rails.env == 'production' ? "https://shielded-plains-9439.herokuapp.com" : "http://localhost:3000"
 
 RTurk::logger.level = Logger::DEBUG
 
